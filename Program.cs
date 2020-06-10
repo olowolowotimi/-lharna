35baf65d608146d0521d84cd71eed8c4bd9898ef
olowolowo timilehin <olowolowotimilehin@yahoo.com>
2020-06-10 12:29:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lharna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Length, Breadth;
+            int Perimeter;
+            Console.WriteLine("enter your length");
+            Length=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your breadth");
+            Breadth=Convert.ToInt32(Console.ReadLine());
+            Perimeter=2 *(Length+Breadth);
+            Console.WriteLine("The perimeter of a rectangle="+ Perimeter);
+        }
+    }
+}
